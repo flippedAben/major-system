023048f2dce9af58a1a00995a729650fa68455aa
Ben Yang <ben.yang@utexas.edu>
2016-11-29 17:08:01 -0600

    First commit

---
+++ makeRegex.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# To help me with major system mnemonics
+# Used with visca.com/regexdict, a regex dictionary
+# 0: s, z
+# 1: t, d
+# 2: n
+# 3: m
+# 4: r
+# 5: l
+# 6: g, j
+# 7: k, c
+# 8: f, v
+# 9: p, b
+import regex_dict
+
+mapping = {'0': 'sz',
+           '1': 'td',
+           '2': 'n',
+           '3': 'm',
+           '4': 'r',
+           '5': 'l',
+           '6': 'gj',
+           '7': 'kc',
+           '8': 'fv',
+           '9': 'pb'}
+
+while 1:
+    stringToConvert = raw_input("Enter number: ")
+    if stringToConvert == "quit":
+        break
+    notReserved = "[^sztdnmrlgjkcfvpb]*"
+    listStrings = [notReserved]
+
+    for digit in stringToConvert:
+        listStrings.append("[" + mapping[digit] + "]" + notReserved)
+
+    fullRegexString = "^" + ''.join(listStrings) + "$"
+    # print fullRegexString
+    regex_dict.printNouns(fullRegexString)
---
+++ regex_dict.py
@@ -0,0 +1,34 @@
+#/usr/bin/python
+import mechanize
+
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.open("http://www.visca.com/regexdict")
+
+# Print all of the forms on the webpage
+# for form in br.forms():
+#     print form
+
+def printWords( string, parts ):
+    br.select_form(nr = 0)
+    br.form['str'] = string
+    br.form['ps'] = parts
+
+    req1 = br.submit()
+
+    result = br.response().read()
+    lines = result.split('\n')
+
+    for line in lines:
+        length = len(line)
+        if "http://www.yourdictionary.com/" in line:
+            print line[length-line[::-1].index('"')+1:length-8]
+
+def printNouns( string ):
+    printWords( string, ['n'] )
+
+def printAdjectives( string ):
+    printWords( string, ['a'] )
+
+def printVerbs( string ):
+    printWords( string, ['v'] )
